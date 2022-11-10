7e5de0fbba8df02e110cd7f42c6791679810f87d
aakansha1234 <20503818+aakansha1234@users.noreply.github.com>
2022-11-11 00:07:48 +0530

    initial contract

---
+++ packages/hardhat/contracts/NFT1on1.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT1on1 is ERC721 {
+
+  constructor() ERC721("1on1", "1on1") {
+    _mint(msg.sender, 1);
+  }
+}
